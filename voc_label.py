a0c707683ef27c468d2ad8175d0b39bdb5c50a16
wang <tianlongwang1994@gmail.com>
2023-04-13 13:37:08 +0800

    增加其他功能脚本

--- voc_label.py
+++ voc_label.py
@@ -14,10 +14,18 @@ sets = ['train', 'test', 'val']
 # classes = ["BianXing","HeiBan","GanTiao","LieKou","HaoZao","NiaoZhuo","LanZao","ShuZao","PoPi",'FengBan']
 # classes = ["BianXing","HeiBan","GanTiao","LieKou","HaoZao","NiaoZhuo","LanZao","ShuZao"]
 # classes = ["AoKeng", "HuaShang"]
-# classes = ["Hao", "AoKeng"]
+# classes = ["label", "cuobiao", "jietou", "posun", "loubai"]
 # classes = ["Dian", "Kuai", "Xian"]
 # classes = ["Dian", "Kuai","Xian", "HanDian"]
-classes = ["Hao", "Qing","Zhong", "Yan"]
+# classes = ["Hao", "Qing","Zhong", "Yan"]
+# classes = ["yewei", "yellow", "ng"]
+# classes = ["yewei", "blue", "ng"]
+# classes = ["xiangchang", "yiwu", "zhangbao", "jiya"]
+# classes = ["feisi","weiyi","kailie","zhenghan","hankuan","wuhan","fanhan","fanduan","left"]
+# classes = ["huashang"]
+classes = ["aokeng"]
+
+
 def convert(size, box):
     dw = 1. / size[0]
     dh = 1. / size[1]
@@ -29,6 +37,8 @@ def convert(size, box):
     w = w * dw
     y = y * dh
     h = h * dh
+
+
     # print(str(x).split(".")[0])
     # print(str(y).split(".")[0])
     #
@@ -45,7 +55,7 @@ def convert(size, box):
         print(x, y, w, h)
     return (x, y, w, h)
  
- 
+
 def convert_annotation(image_id):
     in_file = open('data/Annotations/%s.xml' % (image_id))
     out_file = open('data/labels/%s.txt' % (image_id), 'w')
@@ -54,34 +64,40 @@ def convert_annotation(image_id):
     size = root.find('size')
     w = int(size.find('width').text)
     h = int(size.find('height').text)
- 
+
     for obj in root.iter('object'):
         difficult = obj.find('difficult').text
         cls = obj.find('name').text
+
         if cls not in classes and int(difficult) == 1:
             continue
+
         cls_id = classes.index(cls)
         xmlbox = obj.find('bndbox')
         b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text),
              float(xmlbox.find('ymax').text))
         bb = convert((w, h), b)
+
         out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
  
  
 wd = getcwd()
-print(wd)
+
 dirlist = os.listdir("data/images")
 for image_set in sets:
     if not os.path.exists('data/labels/'):
         os.makedirs('data/labels/')
     image_ids = open('data/ImageSets/%s.txt' % (image_set)).read().strip().split()
     list_file = open('data/%s.txt' % (image_set), 'w')
+    lists_file = open('data/%s_labels.txt' % (image_set), 'w')
     for image_id in image_ids:
         for dirs in dirlist:
             s = dirs.split("."[0])
             if image_id == s[0].replace(" ", ""):
 
                 list_file.write('data/images/%s.%s\n' % (image_id, s[1]))
+                lists_file.write('data/labels/%s.txt\n' % (image_id))
+
                 convert_annotation(image_id)
             else:
                 continue
